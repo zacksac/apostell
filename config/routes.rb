50fe58fd8a3ffc1be3fc6f218680735472acf77f
JC Yawili <jcyawili@hotmail.fr>
2015-09-17 00:53:52 +0200

    my first commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,31 +5,31 @@ Rails.application.routes.draw do
 
   get 'sessions/new'
 
-  get    'sessions/new'
+  get 'sessions/new'
 
-  get     'users/new'
+  get 'users/new'
 
-  root    'static_pages#home'
+  root 'static_pages#home'
 
-  get     'help'    => 'static_pages#help'
+  get 'help'    => 'static_pages#help'
   
-  get     'about'   => 'static_pages#about'
+  get 'about'   => 'static_pages#about'
   
-  get     'contact' => 'static_pages#contact'
+  get 'contact' => 'static_pages#contact'
   
-  get     'signup'  => 'users#new'
+  get 'signup'  => 'users#new'
   
-  get     'login'   => 'sessions#new'
+  get 'login'   => 'sessions#new'
   
-  get     'login'   => 'sessions#create'
+  get 'login'   => 'sessions#create'
   
-  get     'logout'  => 'sessions#destroy'
+  get 'logout'  => 'sessions#destroy'
   
-  get     'login'   => 'sessions#new'
+  get 'login'   => 'sessions#new'
   
-  post    'login'   => 'sessions#create'
+  post 'login'   => 'sessions#create'
   
-  delete  'logout'  => 'sessions#destroy'
+  delete 'logout'  => 'sessions#destroy'
   
   resources :users do
     member do
