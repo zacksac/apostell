259ef7b8fe171aac39c5d6c5ddbff15ba7f464cf
JC Yawili <jcyawili@hotmail.fr>
2015-09-17 04:54:30 +0200

    Replaced FILL_IN with values in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,7 +50,7 @@ class User < ActiveRecord::Base
   
   # Activates an account.
   def activate
-    update_columns(activated: FILL_IN, activated_at: FILL_IN)
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
 
   # Sends activation email.
@@ -61,8 +61,8 @@ class User < ActiveRecord::Base
   # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
-    update_columns(reset_digest:  FILL_IN,
-                   reset_sent_at: FILL_IN)
+    update_columns(reset_digest:  User.digest(reset_token),
+                   reset_sent_at: Time.zone.now)
   end
 
   # Sends password reset email.
