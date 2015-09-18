d4d6778726520f638dd0fc943243e290da449d95
JC Yawili <jcyawili@hotmail.fr>
2015-09-18 18:29:47 +0200

    Final commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+
+  Bundler.require(*Rails.groups(assets: %w(development test production)))
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
