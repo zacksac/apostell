11bd7140d3c14b2c5ad3fc2e527dbe46375c8fc0
JC Yawili <jcyawili@hotmail.fr>
2015-09-20 23:50:26 +0200

    undid previous changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,7 +64,7 @@ class UsersController < ApplicationController
   private
   
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
     
     # Before filters
