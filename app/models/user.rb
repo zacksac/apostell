348a7eb43c1892cbeb97ec589bd9365864f42269
JC Yawili <jcyawili@hotmail.fr>
2015-09-20 23:41:41 +0200

    added paperclicp profile picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_presencE :avatar
+  validates_attachment_size, :data, :less_than => 5.megabytes
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
